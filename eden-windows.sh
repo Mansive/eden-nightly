887d3451a08e275f4039615dacac4f832220e4dc
Mansive <33560917+Mansive@users.noreply.github.com>
2026-01-27 01:10:47 -0600

    Slimming down changes

--- eden-windows.sh
+++ eden-windows.sh
@@ -149,7 +149,6 @@ echo "-- Packing build artifacts..."
 cd bin
 mv -v eden.exe "$EXE_NAME".exe
 ZIP_NAME="$EXE_NAME.zip"
-rm -f *.zip
 7z a -tzip -mx=9 -x!*.zip "$ZIP_NAME" *
 rm -v "$EXE_NAME".exe
 find . -type f -name "*.pdb" -exec rm -v {} +
